db2ce2cd97bb774ab35b71dc8305257190671b9f
Michael Thamm <mikethamm44@gmail.com>
2022-07-26 17:33:08 -0400

    Updates to prepare to finalize GitHub Repo

--- configuration/StartInDocker.bat
+++ configuration/StartInDocker.bat
@@ -4,7 +4,7 @@ docker ps -a
 :: Build Docker image
 docker build -t michaelthamm/github-projects:Flask-Docker .
 :: Run the Docker container in detached mode to allow access to port 5000
-docker run -d -p 5000:5000 michaelthamm:Flask-Docker
+docker run -d -p 5000:5000 michaelthamm/github-projects:Flask-Docker
 :: Set the local webpage location
 set url="http://localhost:5000"
 :: Start webpage in Google Chrome
