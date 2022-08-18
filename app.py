f390640c2c02f75167b2c0b1cb13b8179662a9bd
Michael Thamm <mikethamm44@gmail.com>
2022-08-18 08:57:48 -0400

    Clean up the clone from Flask and create a skeleton application

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import sqlite3
-from src import emailer
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -7,32 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Key value pairs from url after ?
-    content = updatePage()
-
-    # If a URL request has ? followed by key value pairs and there is a method key it was generated with the form's hidden id
-    if request.args.get("method") == "emailer.py":
-        emailer.run(request.args)
-
-    # Run the index HTML code with contents from the database
-    return render_template('index.html', posts=content)
-
-
-def updatePage():
-    # TODO Add the email functionality here
-    # Open the database connection
-    conn = get_db_connection()
-    # Select all entries from posts table and fetch all rows of the query result
-    posts = conn.execute('SELECT * FROM posts').fetchall()
-    conn.close()
-    return posts
-
-
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    # set the row_factory attribute to sqlite3.Row for name-based access to columns
-    conn.row_factory = sqlite3.Row
-    return conn
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
