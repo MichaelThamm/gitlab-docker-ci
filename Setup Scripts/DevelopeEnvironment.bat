000450a3a7d1c50333c8a1ade60f02de5d6f4285
Michael Thamm <michael.thamm@riskine.com>
2022-10-24 10:03:47 +0200

    Updated gitignore and project structure

---
+++ Setup Scripts/DevelopeEnvironment.bat
@@ -0,0 +1,9 @@
+:: Change dir into the project root
+cd ..
+:: Create virtual environment named venv
+pip install virtualenv
+python -m virtualenv venv
+:: Activate the VM
+venv\Scripts\activate
+
+EXIT
