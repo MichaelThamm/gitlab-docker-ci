e2b90f558395ca3a22153f43f863e5f42beabef1
Michael <michael.thamm@riskine.com>
2023-10-11 15:01:28 +0200

    chore: remove .bat files

--- setup-scripts/DevelopeEnvironment.bat
+++
@@ -1,9 +0,0 @@
-:: Change dir into the project root
-cd ..
-:: Create virtual environment named venv
-pip install virtualenv
-python -m virtualenv venv
-:: Activate the VM
-venv\Scripts\activate
-
-EXIT
